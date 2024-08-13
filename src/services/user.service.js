5af84160de3b2ed54805b054367a494419c53b2a
EricWithC04 <negativegame117@gmail.com>
2024-08-12 22:19:50 -0300

    feat: user service

---
+++ src/services/user.service.js
@@ -0,0 +1,26 @@
+import UserModel from "../models/user.model";
+
+export const getAllUsers = async () => {
+    const users = await UserModel.findAll();
+    return users;
+}
+
+export const getUserById = async (id) => {
+    const user = await UserModel.findByPk(id);
+    return user;
+}
+
+export const createUser = async (data) => {
+    const user = await UserModel.create(data);
+    return user;
+}
+
+export const updateUser = async (id, data) => {
+    const user = await UserModel.update(data, { where: { id } });
+    return user;
+}
+
+export const deleteUser = async (id) => {
+    const user = await UserModel.destroy({ where: { id } });
+    return user;
+}
