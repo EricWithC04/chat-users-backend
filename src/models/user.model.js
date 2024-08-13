c7358be9e6b3cd07d38345016449dbfe8689beac
EricWithC04 <negativegame117@gmail.com>
2024-08-12 22:18:13 -0300

    feat: user model

---
+++ src/models/user.model.js
@@ -0,0 +1,34 @@
+import { sequelize } from "../config/db.js";
+import { DataTypes } from "sequelize";
+
+export const UserModel = sequelize.define("users", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    userName: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+export default UserModel
