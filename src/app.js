e817ce230e37e79fbc72be13febed3e11dee9318
EricWithC04 <negativegame117@gmail.com>
2024-08-12 22:00:42 -0300

    feat: middlewares

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 import Express from "express"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+import fileUpload from "express-fileupload"
 
 const app = Express()
+
+app.use(cors())
+app.use(helmet())
+app.use(morgan("dev"))
+app.use(Express.json())
+app.use(fileUpload())
 
 export default app
